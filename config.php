380f451dc12c31098cc085b691bb1e300893ca45
zulazri zulkarnain <zulazrizulkarnain@gmail.com>
2023-08-14 14:29:24 +0800

    fixed

--- config.php
+++ config.php
@@ -5,9 +5,10 @@ $port = 25060; // Replace with your actual port (25060 for SSL, 5432 for non-SSL
 $dbname = 'workforcedb'; // Replace with your actual database name
 $user = 'workforcedb'; // Replace with your actual username
 $password = 'AVNS_R18UHHCcBlRaV75tK8E'; // Replace with your actual password
+$ssl = 'require';
 
 // Construct the connection string
-$connectionString = "host=$host port=$port dbname=$dbname user=$user password=$password";
+$connectionString = "host=$host port=$port dbname=$dbname user=$user password=$password ssl=$ssl";
 
 // Create a PostgreSQL database connection
 $db = pg_connect($connectionString);
