5980b031333e2634e592dc72c4860f50b1b50429
zulazri zulkarnain <zulazrizulkarnain@gmail.com>
2023-08-14 17:20:59 +0800

    fixed

--- config.php
+++ config.php
@@ -2,9 +2,9 @@
 
 $host = 'app-ee404687-ef72-4c54-b18b-ef6f2fcfa48b-do-user-14511752-0.b.db.ondigitalocean.com'; // Replace with your actual host
 $port = 25060; // Replace with your actual port (25060 for SSL, 5432 for non-SSL)
-$dbname = 'defaultdb'; // Replace with your actual database name
-$user = 'doadmin'; // Replace with your actual username
-$password = 'AVNS_jLlQb0aj38CO2ED0jQy'; // Replace with your actual password
+$dbname = 'workforcedb'; // Replace with your actual database name
+$user = 'workforcedb'; // Replace with your actual username
+$password = 'AVNS_R18UHHCcBlRaV75tK8E'; // Replace with your actual password
 $ssl = 'require';
 
 // Construct the connection string
@@ -14,8 +14,7 @@ $connectionString = "host=$host port=$port dbname=$dbname user=$user password=$p
 $db = pg_connect($connectionString);
 
 if (!$db) {
-   // echo "Error : Unable to open database\n";
-   echo "Error : " . pg_last_error($db);
+   echo "Error : Unable to open database\n";
    // die("Connection failed: " . pg_last_error());
 }
 
