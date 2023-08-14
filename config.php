25a8561644fce4f3e278af4e30493bd02f19a72f
zulazri zulkarnain <zulazrizulkarnain@gmail.com>
2023-08-14 14:21:50 +0800

    FIxed connection

--- config.php
+++ config.php
@@ -16,12 +16,23 @@
 
 // $db = pg_connect( "$host $port $dbname $credentials $ssl" );
 
-$conn_string = "host=app-ee404687-ef72-4c54-b18b-ef6f2fcfa48b-do-user-14511752-0.b.db.ondigitalocean.com port=25060 dbname=workforcedb user=workforcedb password=AVNS_R18UHHCcBlRaV75tK8E ssl=require";
-$dbconn4 = pg_connect($conn_string);
+$host = 'app-ee404687-ef72-4c54-b18b-ef6f2fcfa48b-do-user-14511752-0.b.db.ondigitalocean.com'; // Replace with your actual host
+$port = 25060; // Replace with your actual port (25060 for SSL, 5432 for non-SSL)
+$dbname = 'workforcedb'; // Replace with your actual database name
+$user = 'workforcedb'; // Replace with your actual username
+$password = 'AVNS_R18UHHCcBlRaV75tK8E'; // Replace with your actual password
 
-if (!$dbconn4) {
-   echo "Error : Unable to open database\n";
-} else {
-   echo "Opened database successfully\n";
+// Construct the connection string
+$connectionString = "host=$host port=$port dbname=$dbname user=$user password=$password";
+
+// Create a PostgreSQL database connection
+$db = pg_connect($connectionString);
+
+if (!$db) {
+    die("Connection failed: " . pg_last_error());
 }
+
+// Close the connection when done
+pg_close($db);
+
 ?>
