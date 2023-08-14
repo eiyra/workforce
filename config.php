774654ccff10114329bd6136f9da80d2650a2369
zulazri zulkarnain <zulazrizulkarnain@gmail.com>
2023-08-14 19:31:15 +0800

    fixed

--- config.php
+++ config.php
@@ -1,14 +1,16 @@
 <?php
 
-$con = mysqli_connect("localhost","admin","89b6d4e307ed5b88a35e1ba6600416f8d3062c2eeebb82fb","workforce");
+$host = "localhost";
+$username = "root";
+$password = "";
+$dbname = "workforce";
 
-// Check connection
-if (mysqli_connect_errno())
-  {
-	  echo "Failed to connect to MySQL: " . mysqli_connect_error();
-	  echo "<meta http-equiv=\"refresh\" content=\"3;URL=../index\">";
-  }
+$con = mysqli_connect($host, $username, $password, $dbname);
 
+if (!$con) {
+   echo ("Cannot connect to database");
+   die("Failed to connect to MySQL: " . mysqli_connect_error());
+}
 
 // $host = 'app-ee404687-ef72-4c54-b18b-ef6f2fcfa48b-do-user-14511752-0.b.db.ondigitalocean.com'; // Replace with your actual host
 // $port = 25060; // Replace with your actual port (25060 for SSL, 5432 for non-SSL)
